a8bc37e28ccbe37235c7d1fe8dcf86edf51e45d1
asmita-rgb <asmitan97@gmail.com>
2020-08-20 17:06:33 +0600

    true/false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//false:
+//0
+//""
+//undefined
+//null
+//NaN
+//truth:
+//'0'
+//' '
+//[]
+//{}
+
+let name = 12;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
